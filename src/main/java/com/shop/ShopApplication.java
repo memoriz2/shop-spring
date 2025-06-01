a3a7ed2c7bf5aeb38a7152e61c484a3140e26811
jseo <ahndjds@gmail.com>
2025-06-01 19:32:32 +0900

    db 연결설정 주석2

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -3,13 +3,18 @@ package com.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
 @ComponentScan(basePackages = "com.shop")
-@EntityScan("com.shop.entity")
-@EnableJpaRepositories("com.shop.repository")
+// @EntityScan("com.shop.entity")
+// @EnableJpaRepositories("com.shop.repository")
 public class ShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
