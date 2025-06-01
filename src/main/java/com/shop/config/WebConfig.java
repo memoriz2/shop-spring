fa3b76ae3dee1762d631e609a873f288c13b826a
jseo <ahndjds@gmail.com>
2025-06-01 10:52:54 +0900

    관리자 url 변경

--- src/main/java/com/shop/config/WebConfig.java
+++ src/main/java/com/shop/config/WebConfig.java
@@ -9,7 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://shop-tiqaktmxj.vercel.app")
+                .allowedOrigins(
+                    "https://shop-tiqaktmxj.vercel.app",
+                    "https://shop-tiqaktmxj-dfp3ekhhx-memoriz2s-projects.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
