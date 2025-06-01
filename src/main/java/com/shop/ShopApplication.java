5c0c45ade5871a1471c7028cbd8b787abb9e1dd4
jseo <ahndjds@gmail.com>
2025-06-01 20:12:00 +0900

    db 연결 설정 수정2

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.shop.entity")
