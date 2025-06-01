ca36b688ccedac03b73eeb26284851f101ba411d
jseo <ahndjds@gmail.com>
2025-06-01 20:37:34 +0900

    db 연결 활성화

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -2,15 +2,12 @@ package com.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {
-    DataSourceAutoConfiguration.class,
-    HibernateJpaAutoConfiguration.class
-})
-@ComponentScan(basePackages = "com.shop")
+@SpringBootApplication
+@EntityScan(basePackages = "com.shop.entity")
+@EnableJpaRepositories(basePackages = "com.shop.repository")
 public class ShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
